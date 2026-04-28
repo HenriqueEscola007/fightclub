40051c597e0984e90c988db7ab2037dadd128c55
Henrique da Silva <da.silva.henrique1407@escola.pr.gov.br>
2026-04-28 13:58:57 -0300

    perguntei o nome do personagem e comecei o jogo

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("oi")
+def personagem(nome):
+    return f"Olá lutador, {nome}!"
+nome = input("Qual será o nome do seu personagem?\n")
+print(personagem(nome))
+print("Bem vindo ao\nFIGHT CLUB")
+escolha = input("  START\n  QUIT\n")
+while escolha == "START":
+    if escolha == "QUIT":
+        break
+    print("Começando")
