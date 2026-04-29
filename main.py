009be3c000f273bae31d18794704eee18829b465
Henrique da Silva <da.silva.henrique1407@escola.pr.gov.br>
2026-04-29 13:28:03 -0300

    colocado a selecao de dificuldade

--- main.py
+++ main.py
@@ -5,10 +5,19 @@ nome = input("Qual será o nome do seu personagem?\n")
 print(personagem(nome))
 print("Bem vindo ao\nFIGHT CLUB")
 
-while True:
-    escolha = int(input("1 - START\n  2 - QUIT\n"))
-    if escolha == 2:
-        break
-    else :
-        print("Começando")
-        input("escolha o nivel da jogabilidade\nFACIL\nNORMAL\nDIFICIL\n")
+escolha = int(input("1 - START\n2 - QUIT\n"))
+if escolha == 2:
+    print("Tchau")
+else :
+    print("Começando")
+    modo = str(input("escolha o nivel da jogabilidade\nFACIL\nNORMAL\nDIFICIL\n"))
+    
+    if modo == "facil":
+        dano_inimigo = 20
+        vida_lutador = 100
+    elif modo == "normal":
+        dano_inimigo = 30
+        vida_lutador = 90
+    elif modo == "dificil":
+        dano_inimigo = 40
+        vida_lutador = 80
